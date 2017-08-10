def2890b80aa13641b66d283879b183c27201f91
Kayvan Sylvan <kayvansylvan@gmail.com>
2017-08-10 00:02:46 -0700

    Fixes for Contact form at /contact

--- roles/mailserver/files/contact/index.php
+++ roles/mailserver/files/contact/index.php
@@ -86,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php endif; ?>
 
     <div class="col-md-6 col-md-offset-3">
-        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" enctype="application/x-www-form-urlencoded" id="contact-form" class="form-horizontal" method="post">
+        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>{{ contact_form_postfix }}" enctype="application/x-www-form-urlencoded" id="contact-form" class="form-horizontal" method="post">
             <div class="form-group">
                 <label for="form-name" class="col-lg-2 control-label"><?php echo $config->get('fields.name'); ?></label>
                 <div class="col-lg-10">
